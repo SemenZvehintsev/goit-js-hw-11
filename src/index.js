f59d817176599f215213a0add5811da87a1a7aaf
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-21 17:02:08 +0200

    added changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import axios from 'axios';
 
 
+const inputText = document.querySelector('input')
+const formSearch = document.querySelector('.search-form')
+const gallery = document.querySelector('.gallery')
 
+formSearch.addEventListener('submit', searchImgs)
+const key = '31495001-d7fca89852a5b5217d905cd4a';
+function searchImgs(event) {
+    event.preventDefault()
+    const searchImg = inputText.value
+    axios.get(`https://pixabay.com/api/?key=${key}=${searchImg}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message))
+
+}
 
 // <div class="photo-card">
 //   <img src="" alt="" loading="lazy" />
