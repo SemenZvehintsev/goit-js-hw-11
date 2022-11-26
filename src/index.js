45412ffa0acb6a108fc964981cdd8a3b9139fa2b
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-26 17:26:30 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,75 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import axios from 'axios';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const inputText = document.querySelector('input')
 const formSearch = document.querySelector('.search-form')
 const gallery = document.querySelector('.gallery')
 
 formSearch.addEventListener('submit', searchImgs)
+
 const key = '31495001-d7fca89852a5b5217d905cd4a';
+
+let pageNumber = 0;
+
 function searchImgs(event) {
+    pageNumber += 1;
     event.preventDefault()
-    const searchImg = inputText.value
-    axios.get(`https://pixabay.com/api/?key=${key}=${searchImg}&image_type=photo&pretty=true`)
-    .then(response => response.json())
-    .then(data => console.log(data))
+    const searchImg = inputText.value.split(' ').join('+')
+    console.log(searchImg)
+    axios.get(`https://pixabay.com/api/?key=${key}q=${searchImg}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=40`)
+    .then(response => {
+        console.log(response.data)
+        console.log(response.statusText)
+        return response.data})
+    .then(headers => headers.hits)
+    .then(hits => onlyOneFound(hits))
     .catch(error => console.log(error.message))
 
 }
+
+
+function onlyOneFound(data) {
+    gallery.innerHTML=''
+    if (data.length > 0) {
+        markup = data.map(image => 
+            `<a href="${image.largeImageURL}" style="text-decoration: none" >
+                <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /> 
+                <div class="info" style="display: flex" style="gap: 20px" style="padding: 5px">
+                    <p class="info-item" style="margin: 10px">
+                        <b>Likes</b>
+                        </br>
+                        ${image.likes}
+                    </p>
+                    <p class="info-item" style="margin: 10px">
+                        <b>Views</b>
+                        </br>               
+                        ${image.views}
+                    </p>
+                    <p class="info-item" style="margin: 10px">
+                        <b>Comments</b>
+                        </br>
+                        ${image.comments}
+                    </p>
+                    <p class="info-item" style="margin: 10px">
+                        <b>Downloads</b>
+                        </br>
+                        ${image.downloads}
+                    </p>
+                </div>
+              </a>`
+            ).join('')
+            gallery.insertAdjacentHTML('afterbegin', markup);
+
+            
+    } 
+    
+
+}
+
+let lightbox = new SimpleLightbox('.gallery a', { });
 
 // <div class="photo-card">
 //   <img src="" alt="" loading="lazy" />
